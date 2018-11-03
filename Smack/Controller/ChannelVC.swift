154dc1b6880c19cbc893bd2faba6485414718e0e
exxe <exxe06@MarcTerencesMBP.fritz.box>
2018-11-03 20:30:16 +0100

    end of lesson 2

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,30 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by exxe on 03.11.18.
+//  Copyright © 2018 exxe. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
