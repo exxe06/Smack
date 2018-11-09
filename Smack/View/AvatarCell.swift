0d39949b2fee3d44687650ec10f683c0279834a3
exxe <exxe06@MarcTerencesMBP.fritz.box>
2018-11-09 21:31:21 +0100

    end of lesson11

---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,27 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by exxe on 09.11.18.
+//  Copyright © 2018 exxe. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    
+    func setupView() {
+        self.layer.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+}
