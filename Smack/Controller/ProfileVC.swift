c6eae1103a72301def060ba5a96ea440db3a7284
exxe <exxe06@MarcTerencesMBP.fritz.box>
2018-11-14 18:48:56 +0100

    end of lesson14

---
+++ Smack/Controller/ProfileVC.swift
@@ -0,0 +1,53 @@
+//
+//  ProfileVC.swift
+//  Smack
+//
+//  Created by exxe on 14.11.18.
+//  Copyright © 2018 exxe. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    // Outlets
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var bgView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+
+    @IBAction func closeModelPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func logoutPressed(_ sender: Any) {
+        UserDataService.instance.logoutUser()
+        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView() {
+        profileImg.image = UIImage(named: UserDataService.instance.avatarName)
+        profileImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        userName.text = "\(UserDataService.instance.name)"
+        userEmail.text = "\(UserDataService.instance.email)"
+        
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(ProfileVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
