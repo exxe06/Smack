1f83bd8e5e199522d8bdb175e53d4aff8592d909
exxe <exxe06@MarcTerencesMBP.fritz.box>
2018-11-20 15:21:08 +0100

    end of lesson 16

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -32,7 +32,7 @@ class MessageService {
 
                         self.channels.append(channel)
                     }
-                    print(self.channels[0].channelTitle)
+//                    print(self.channels[0].channelTitle)
                     completion(true)
                 }
                 
