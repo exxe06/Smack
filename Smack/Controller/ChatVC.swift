2a8dd5be84533a56f9a5da70671b71a4e807cb64
exxe <exxe06@MarcTerencesMBP.fritz.box>
2018-11-20 18:48:28 +0100

    end of lesson20

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -13,10 +13,13 @@ class ChatVC: UIViewController {
     // Outlets
     @IBOutlet weak var menuBtn: UIButton!
     @IBOutlet weak var channelNameLbl: UILabel!
+    @IBOutlet weak var messagesTextBox: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.bindToKeyboard()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ChatVC.handleTap))
+        view.addGestureRecognizer(tap)
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
@@ -44,11 +47,30 @@ class ChatVC: UIViewController {
         updateWithChannel()
     }
     
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
+    
     func updateWithChannel() {
         let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
         channelNameLbl.text = "#\(channelName)"
         getMessages()
     }
+    
+    @IBAction func sendMsgPressed(_ sender: Any) {
+        if AuthService.instance.isLoggedIn {
+            guard let channelId = MessageService.instance.selectedChannel?.id else { return }
+            guard let message = messagesTextBox.text else { return }
+            
+            SocketService.instance.addMessage(messageBody: message, userId: UserDataService.instance.id, channelId: channelId) { (success) in
+                if success {
+                    self.messagesTextBox.text = ""
+                    self.messagesTextBox.resignFirstResponder()
+                }
+            }
+        }
+    }
+    
     
     
     func onLoginGetMessages() {
