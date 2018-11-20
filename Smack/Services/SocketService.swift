491ea8b94b5d081a80111786019d37446d93d99a
exxe <exxe06@MarcTerencesMBP.fritz.box>
2018-11-20 16:24:49 +0100

    end of lesson 17

---
+++ Smack/Services/SocketService.swift
@@ -0,0 +1,56 @@
+//
+//  SocketService.swift
+//  Smack
+//
+//  Created by exxe on 20.11.18.
+//  Copyright © 2018 exxe. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+
+    static let instance = SocketService()
+    
+    let manager: SocketService
+    let socket: SocketIOClient
+    
+    override init() {
+        self.manager = Socket
+        super.init()
+        
+
+    }
+        
+
+    func establishConnection() {
+        socket.connect()
+        
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        socket.on("channelCreated") { (dataArray, ack) in
+            guard let channelName = dataArray[0] as? String else { return }
+            guard let channelDesc = dataArray[1] as? String else { return }
+            guard let channelId = dataArray[2] as? String else { return }
+            
+            let newChannel = Channel(channelTitle: channelName, channelDescription: channelDesc, id: channelId)
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+            
+        }
+        
+    }
+    
+    
+}
